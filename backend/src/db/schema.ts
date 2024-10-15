087f9cc73f15a61079ecb65c0ff7afd4162930c5
OussamaAtifii <oussamaati03@gmail.com>
2024-10-15 19:12:53 +0200

    feat: add banks table and relation with users

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,5 +1,5 @@
-import { sql } from 'drizzle-orm';
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { relations, sql } from 'drizzle-orm';
+import { integer, real, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const usersTable = sqliteTable('users', {
   id: integer('id').primaryKey(),
@@ -20,3 +20,33 @@ export const usersTable = sqliteTable('users', {
 
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
+
+export const usersRelations = relations(usersTable, ({ many }) => ({
+  banks: many(banksTable),
+}));
+
+export const banksTable = sqliteTable('banks', {
+  id: integer('id').primaryKey(),
+  name: text('name').notNull(),
+  totalSavings: real('total_savings').notNull().default(0),
+  description: text('description'),
+  goal: real('goal').notNull().default(0),
+  visibility: integer('visibility', { mode: 'boolean' }).default(true),
+  userId: integer('user_id').notNull(),
+  createdAt: text('created_at')
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text('updated_at')
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+});
+
+export type InsertBank = typeof banksTable.$inferInsert;
+export type SelectBank = typeof banksTable.$inferSelect;
+
+export const banksRelations = relations(banksTable, ({ one }) => ({
+  user: one(usersTable, {
+    fields: [banksTable.userId],
+    references: [usersTable.id],
+  }),
+}));
