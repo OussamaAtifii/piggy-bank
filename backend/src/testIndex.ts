55d7c39d9827ac064bae331e65ff15538952f1cc
Alexrsnchz <alexrsnchz@gmail.com>
2024-10-19 18:47:27 +0200

    test(backend): Test initial config

---
+++ backend/src/testIndex.ts
@@ -0,0 +1,25 @@
+import express, { Express, NextFunction, Request, Response } from 'express';
+import { PORT } from './config/config';
+import { corsMiddleware } from './middlewares/corsMiddleware';
+import cookieParser from 'cookie-parser';
+import userRouter from './routes/users';
+import bankRouter from './routes/banks';
+import tagRouter from './routes/tags';
+
+const app: Express = express();
+
+app.disable('x-powered-by');
+
+app.use(express.json());
+app.use(corsMiddleware);
+app.use(cookieParser());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send('Se viene Piggy-Bank pa.');
+});
+
+app.use('/user', userRouter);
+app.use('/bank', bankRouter);
+app.use('/tag', tagRouter);
+
+export default app;
