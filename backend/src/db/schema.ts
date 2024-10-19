a6b69269ca2ad1504ac1cf5d8128462decd7b74f
OussamaAtifii <oussamaati03@gmail.com>
2024-10-19 14:00:39 +0200

    feat(tags): add tags table and make names not unique

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,6 +1,7 @@
 import { relations, sql } from 'drizzle-orm';
 import { integer, real, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
+// Users table
 export const usersTable = sqliteTable('users', {
   id: integer('id').primaryKey(),
   username: text('username').unique().notNull(),
@@ -21,13 +22,16 @@ export const usersTable = sqliteTable('users', {
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
 
+// Users relations
 export const usersRelations = relations(usersTable, ({ many }) => ({
   banks: many(banksTable),
 }));
 
+// -----------------------------------------------------------------------------
+// Banks table
 export const banksTable = sqliteTable('banks', {
   id: integer('id').primaryKey(),
-  name: text('name').unique().notNull(),
+  name: text('name').notNull(),
   totalSavings: real('total_savings').notNull().default(0),
   description: text('description'),
   goal: real('goal').notNull().default(0),
@@ -44,9 +48,38 @@ export const banksTable = sqliteTable('banks', {
 export type InsertBank = typeof banksTable.$inferInsert;
 export type SelectBank = typeof banksTable.$inferSelect;
 
-export const banksRelations = relations(banksTable, ({ one }) => ({
+// Banks relations
+export const banksRelations = relations(banksTable, ({ one, many }) => ({
   user: one(usersTable, {
     fields: [banksTable.userId],
     references: [usersTable.id],
+  }),
+  tags: many(tagsTable),
+}));
+
+// -----------------------------------------------------------------------------
+// Tags table
+export const tagsTable = sqliteTable('tags', {
+  id: integer('id').primaryKey(),
+  name: text('name').notNull(),
+  description: text('description'),
+  color: text('color').notNull().default('#FFFFFF'),
+  bankId: integer('bank_id').notNull(),
+  createdAt: text('created_at')
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text('updated_at')
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+});
+
+export type InsertTag = typeof tagsTable.$inferInsert;
+export type SelectTag = typeof tagsTable.$inferSelect;
+
+// Tags relations
+export const tagsRelations = relations(tagsTable, ({ one }) => ({
+  bank: one(banksTable, {
+    fields: [tagsTable.bankId],
+    references: [banksTable.id],
   }),
 }));
